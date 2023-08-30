1804cd8233d7b0bcb0b25dd8a318edb987e9303a
GOLDER303 <szymonstepien@interia.pl>
2023-08-30 18:44:25 +0200

    Replace spaces with tabs

--- player.gd
+++ player.gd
@@ -4,37 +4,38 @@ extends Area2D
 var screen_size = Vector2.ZERO
 
 func _ready():
-    screen_size = get_viewport_rect().size
+	screen_size = get_viewport_rect().size
 
 func _process(delta):
-    var direction = Vector2.ZERO
+	var direction = Vector2.ZERO
 
-    if Input.is_action_pressed("move_right"):
-        direction.x += 1
-    if Input.is_action_pressed("move_left"):
-        direction.x -= 1
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
 
-    if Input.is_action_pressed("move_down"):
-        direction.y += 1
-    if Input.is_action_pressed("move_up"):
-        direction.y -= 1
+	if Input.is_action_pressed("move_down"):
+		direction.y += 1
+	if Input.is_action_pressed("move_up"):
+		direction.y -= 1
 
-    direction = direction.normalized()
-    position += direction * speed * delta
+	direction = direction.normalized()
+	position += direction * speed * delta
 
-    position.x = clamp(position.x, 0, screen_size.x)
-    position.y = clamp(position.y, 0, screen_size.y)
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
 
-    if direction.length() > 0:
-        $AnimatedSprite2D.play()
-    else:
-        $AnimatedSprite2D.stop()
+	if direction.length() > 0:
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
 
-    if direction.x != 0:
-        $AnimatedSprite2D.animation = "right"
-        $AnimatedSprite2D.flip_h = direction.x < 0
-        $AnimatedSprite2D.flip_v = false
+	if direction.x != 0:
+		$AnimatedSprite2D.animation = "right"
+		$AnimatedSprite2D.flip_h = direction.x < 0
+		$AnimatedSprite2D.flip_v = false
 
-    elif direction.y != 0:
-        $AnimatedSprite2D.animation = "up"
-        $AnimatedSprite2D.flip_v = direction.y > 0
+	elif direction.y != 0:
+		$AnimatedSprite2D.animation = "up"
+		$AnimatedSprite2D.flip_v = direction.y > 0
+
